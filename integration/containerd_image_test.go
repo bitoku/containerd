44ec0c9b9a185471a76e5451ec7e261aad880eca
Ayato Tokubi <atokubi@redhat.com>
2023-11-21 16:38:46 +0900

    CRI: fix updateImage to show digest in cri server
    Signed-off-by: Ayato Tokubi <atokubi@redhat.com>

--- integration/containerd_image_test.go
+++ integration/containerd_image_test.go
@@ -89,7 +89,7 @@ func TestContainerdImage(t *testing.T) {
 		if img.RepoTags[0] != testImage {
 			return false, fmt.Errorf("unexpected repotag %q", img.RepoTags[0])
 		}
-		if len(img.RepoDigests) != 1 || img.RepoDigests[0] != "gcr.io/library/busybox@sha256:7b3ccabffc97de872a30dfd234fd972a66d247c8cfc69b0550f276481852627c" {
+		if len(img.RepoDigests) != 1 || img.RepoDigests[0] != "ghcr.io/containerd/busybox@sha256:7b3ccabffc97de872a30dfd234fd972a66d247c8cfc69b0550f276481852627c" {
 			return false, fmt.Errorf("unexpected repodigests: %+v", img.RepoDigests)
 		}
 		repoDigest = img.RepoDigests[0]
